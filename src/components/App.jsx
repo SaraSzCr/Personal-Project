be23ad755d0c18783f7db1b6a9b53e5a960f3c71
Sara Sanchez Crespo <saritasc89@gmail.com>
2024-02-21 16:38:49 +0100

    install node modules and start html landing page

---
+++ src/components/App.jsx
@@ -0,0 +1,25 @@
+import '../scss/App.scss'
+
+function App() {
+  return (
+    <div>
+          {/* <img src="" alt="Imagen Hero" /> */}
+      <section>
+        <h1>Cocinar con lo que tengo</h1>
+        <p>¿No sabes qué comer? ¿No tienes tiempo para ir hoy a la compra?
+          Con esta app, vamos a crear recetas fáciles ¡con los ingredientes que ya tienes en casa!</p>
+      </section>
+        <button>Empezar</button>
+
+
+        <header>
+
+        </header>
+
+        <main></main>
+        <footer></footer>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/LandingApp.jsx
@@ -0,0 +1,20 @@
+function LandingApp() {
+
+  return (
+    <div>
+      {/* <img src="" alt="Imagen Hero" /> */}
+      <section>
+        <h1>Cocinar con lo que tengo</h1>
+        <p>¿No sabes qué comer? ¿No tienes tiempo para ir hoy a la compra?
+          Con esta app, vamos a crear recetas fáciles ¡con los ingredientes que ya tienes en casa!</p>
+      </section>
+        <button>Empezar</button>
+        
+    </div>
+
+
+    
+  );
+}
+
+export default LandingApp;
