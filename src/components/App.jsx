f28bd826c12e5f851c62605603a04909ec8b769c
Sara Sanchez Crespo <saritasc89@gmail.com>
2024-02-22 15:48:11 +0100

    add HTML

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,25 +1,55 @@
-import '../scss/App.scss'
+import "../scss/App.scss";
 
 function App() {
   return (
     <div>
-          {/* <img src="" alt="Imagen Hero" /> */}
-      <section>
-        <h1>Cocinar con lo que tengo</h1>
-        <p>¿No sabes qué comer? ¿No tienes tiempo para ir hoy a la compra?
-          Con esta app, vamos a crear recetas fáciles ¡con los ingredientes que ya tienes en casa!</p>
-      </section>
-        <button>Empezar</button>
-
-
-        <header>
-
+      {/* <img src="" alt="Imagen Hero" /> */}
+      
+        <header className="header">
+          <h1 className="header__title">Cocinar con lo que tengo</h1>
         </header>
+        <section>
+        <p>
+          ¿No sabes qué comer? ¿No tienes tiempo para ir hoy a la compra? Con
+          esta app, vamos a crear recetas fáciles ¡con los ingredientes que ya
+          tienes en casa!
+        </p>
+        <a href="">Empezar (siguiente pagina)</a>
+      </section>
+     
 
-        <main></main>
-        <footer></footer>
+      <header className="header">
+        <h1 className="header__title">Cocinar con lo que tengo</h1>
+      </header>
+
+      <main className="main">
+        <section className="main__sectionOne">
+          <form className="main__sectionOne--form">
+            <label className="main__sectionOne--form--label" htmlFor="text">
+              Añade tus ingredientes uno a uno
+            </label>
+            <input
+              className="main__sectionOne--form--input"
+              type="text"
+              name="text"
+              id="text"
+              placeholder="Ej:Zanahoria"
+            />
+            <input className="main__sectionOne--form--button" type="submit" />
+          </form>
+        </section>
+        <section className="main__sectionTwo">
+          <p className="main__sectionTwo--selectedIngredientes">
+            Ingredientes seleccionados:
+          </p>
+        </section>
+        <div className="main__sectionThree">
+          <button className="main__sectionThree--btn" >Buscar recetas</button>
+        </div>
+      </main>
+      <footer className="footer">&copy; by SaraSzCr</footer>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/LandingApp.jsx
+++ src/components/LandingApp.jsx
@@ -8,7 +8,8 @@ function LandingApp() {
         <p>¿No sabes qué comer? ¿No tienes tiempo para ir hoy a la compra?
           Con esta app, vamos a crear recetas fáciles ¡con los ingredientes que ya tienes en casa!</p>
       </section>
-        <button>Empezar</button>
+        {/* <button>Empezar</button> */}
+        
         
     </div>
 
