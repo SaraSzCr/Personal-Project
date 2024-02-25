8cd1c45503f3befe86927ddb86673da97f8b5799
Sara Sanchez Crespo <saritasc89@gmail.com>
2024-02-25 11:00:45 +0100

    styles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,22 +4,22 @@ function App() {
   return (
     <div>
       {/* <img src="" alt="Imagen Hero" /> */}
-      
-        <header className="header">
-          <h1 className="header__title">Cocinar con lo que tengo</h1>
-        </header>
-        
-        <main className="main">
-          <p className="main__desc">
-            ¿No sabes qué comer? ¿No tienes tiempo para ir hoy a la compra? Con
-            esta app, vamos a crear recetas fáciles ¡con los ingredientes que ya
-            tienes en casa!
-          </p>
-          <a className="main__startBtn" href="">Empezar </a> 
-          {/* (siguiente pagina) */}
-        </main>
 
-     
+      <header className="header">
+        <h1 className="header__title">Cocinar con lo que tengo</h1>
+      </header>
+
+      <main className="mainLanding">
+        <p className="mainLanding__desc">
+          ¿No sabes qué comer? ¿No tienes tiempo para ir hoy a la compra? Con
+          esta app, vamos a crear recetas fáciles ¡con los ingredientes que ya
+          tienes en casa!
+        </p>
+        <a className="mainLanding__startBtn" href="">
+          Empezar{" "}
+        </a>
+        {/* (siguiente pagina) */}
+      </main>
 
       <header className="header">
         <h1 className="header__title">Cocinar con lo que tengo</h1>
@@ -31,23 +31,30 @@ function App() {
             <label className="main__sectionOne--form--label" htmlFor="text">
               Añade tus ingredientes uno a uno
             </label>
-            <input
-              className="main__sectionOne--form--input"
-              type="text"
-              name="text"
-              id="text"
-              placeholder="Ej:Zanahoria"
-            />
-            <input className="main__sectionOne--form--button" type="submit" />
+            <div className="main__sectionOne--form--container">
+              <input
+                className="main__sectionOne--form--container--input"
+                type="text"
+                name="text"
+                id="text"
+                placeholder="Ej:Zanahoria"
+              />
+              <input
+                className="main__sectionOne--form--container--button"
+                type="submit"
+                value="Añadir"
+              />
+            </div>
           </form>
         </section>
         <section className="main__sectionTwo">
-          <p className="main__sectionTwo--selectedIngredientes">
+          <p className="main__sectionTwo--selectedIngredients">
             Ingredientes seleccionados:
           </p>
+          <div className="main__sectionTwo--box" ></div>
         </section>
         <div className="main__sectionThree">
-          <button className="main__sectionThree--btn" >Buscar recetas</button>
+          <button className="main__sectionThree--btn">Buscar recetas</button>
         </div>
       </main>
       <footer className="footer">&copy; by SaraSzCr</footer>
